d00032addd62f4548b6e1fea23247fd9b8248901
t.r <mail.magazinn@gmail.com>
2021-01-29 18:03:12 +0900

    Add getAllTasks

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,15 +7,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+        )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String title;
+    private Integer id;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -26,12 +34,12 @@ public class Task {
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    public String getTitle() {
-        return title;
+    public Integer getId() {
+        return id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getContent() {
